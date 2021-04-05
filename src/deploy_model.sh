015742df692684eb1009b575afa567bddd062890
Nishkarsh Raj <nishkarshraj000@gmail.com>
2021-04-05 13:31:32 +0530

    Update deploy_model.sh

--- src/deploy_model.sh
+++ src/deploy_model.sh
@@ -5,4 +5,4 @@ echo "Deploying Production model name=PowerForecastingModel"
 # Set enviorment variable for the tracking URL where the Model Registry is
 export MLFLOW_TRACKING_URI=sqlite:///mlruns.db
 # Serve the production model from the model registry
-mlflow models serve --model-uri models:/PowerForecastingModel/production --no-conda
+mlflow models serve --model-uri models:/PowerForecastingModel/production --no-conda -p 3000
