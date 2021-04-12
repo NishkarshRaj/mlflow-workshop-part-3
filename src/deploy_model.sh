e1d420090c5a7e8d108aac9e38bc74f5407a915b
NishkarshRaj <nishkarshraj000@gmail.com>
2021-04-12 11:49:09 +0530

    Working

--- src/deploy_model.sh
+++ src/deploy_model.sh
@@ -3,6 +3,6 @@
 echo "Deploying Production model name=PowerForecastingModel"
 
 # Set enviorment variable for the tracking URL where the Model Registry is
-export MLFLOW_TRACKING_URI=mysql+pymysql://nishkarsh:nishkarsh@nishkarsh.cmuzdwd6qin5.ap-south-1.rds.amazonaws.com:3306/nishkarsh
+#export MLFLOW_TRACKING_URI=mysql+pymysql://nishkarsh:nishkarsh@nishkarsh.cmuzdwd6qin5.ap-south-1.rds.amazonaws.com:3306/nishkarsh
 # Serve the production model from the model registry
 mlflow models serve --model-uri models:/PowerForecastingModel/production --no-conda -p 3000
